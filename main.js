d1bdd683989c3356c570b2ca26b7e528e5a99872
Kevin Atherton <katherto@gmail.com>
2020-02-26 13:46:54 -0600

    First pass translating JS to Vue.js

--- main.js
+++ main.js
@@ -1,48 +1,51 @@
-function geoFindMe() {
-  const jupiter = {
-    location: 'Kansas State Capitol Visitor Center',
-    position: {
-      lat: 39.048257,
-      lng: -95.677630
-    }
-  };
-
-  const status = document.querySelector('#status');
-
-  function success(position) {
-    const mk2 = {
+new Vue({
+  el: '#app',
+  data: {
+    gettingLocation: false,
+    errorStr: null,
+    jupiter:  {
+      location: 'Kansas State Capitol Visitor Center',
       position: {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
+        lat: 39.048257,
+        lng: -95.677630
       }
+    },
+    userMark: {
+      position: {
+        lat: null,
+        lng: null
+      }
+    },
+    distance: null
+  },
+  created() {
+    // Checks for browser Geolocation support
+    if(!("geolocation" in navigator)) {
+      this.errorStr = 'Geolocation is not supported by your browser.';
+      return;
     }
 
-    const distance = haversine_distance(jupiter, mk2);
-    status.textContent = `You are ${distance.toFixed(2)} miles away from Jupiter (${jupiter.location})`;  // Straight line distance between two lat/long coords in miles
+    this.gettingLocation = true;
+    navigator.geolocation.getCurrentPosition(pos => {
+      this.gettingLocation = false;
+      this.userMark.position.lat = pos.coords.latitude;
+      this.userMark.position.lng = pos.coords.longitude;
+    }, err => {
+      this.gettingLocation = false;
+      this.errorStr = err.message;
+    })
+  },
+  methods: {
+    // Haversine formula for calculating distance between two lat/long coordinates
+    haversineDistance() {
+      var R = 3958.8; // Radius of the Earth in miles, 6371.0710 Radius of the Earth in Kilometers
+      var rlat1 = this.jupiter.position.lat * (Math.PI/180); // Convert degrees to radians
+      var rlat2 = this.userMark.position.lat * (Math.PI/180); // Convert degrees to radians
+      var difflat = rlat2-rlat1; // Radian difference (latitudes)
+      var difflon = (this.userMark.position.lng-this.jupiter.position.lng) * (Math.PI/180); // Radian difference (longitudes)
+    
+      var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+      this.distance = d;
+    }
   }
-
-  function error() {
-    status.textContent = 'Unable to retrieve your location';
-  }
-
-  if (!navigator.geolocation) {
-    status.textContent = 'Geolocation is not supported by your browser';
-  } else {
-    status.textContent = 'Locating...';
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
-}
-
-// Haversine formula for calculating distance between two lat/long coordinates
-function haversine_distance(mk1, mk2) {
-  var R = 3958.8; // Radius of the Earth in miles, 6371.0710 Radius of the Earth in Kilometers
-  var rlat1 = mk1.position.lat * (Math.PI/180); // Convert degrees to radians
-  var rlat2 = mk2.position.lat * (Math.PI/180); // Convert degrees to radians
-  var difflat = rlat2-rlat1; // Radian difference (latitudes)
-  var difflon = (mk2.position.lng-mk1.position.lng) * (Math.PI/180); // Radian difference (longitudes)
-
-  var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
-  return d;
-}
-
-document.querySelector('#find-me').addEventListener('click', geoFindMe);
+});
