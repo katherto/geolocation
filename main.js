9604625255e6330f0dcb0080fa8a29c201e49ad0
Alex Tate <alexander.tate4@gmail.com>
2020-03-03 15:28:06 -0600

    added data for potential planets on I70.

--- main.js
+++ main.js
@@ -3,11 +3,85 @@ new Vue({
   data: {
     gettingLocation: false,
     errorStr: null,
+    planets: {}, //Code that controls planet variables...
+    mercury:  {
+        location: 'Lackman Library',
+        position: {
+          lat: 38.581216,
+          lng: -94.455392, 
+          },
+        location: 'Johnny\'s Tavern',
+        position: {
+          lat: 38.562496,
+          lng: -94.475359, 
+          }
+      },
+    venus:  {
+        location: 'Shawnee City Hall',
+        position: {
+          lat: 39.12434,
+          lng: -94.425528,
+        },
+        location: 'Honeywell Aerospace',
+        position: {
+          lat: 38.561806,
+          lng: -94.512330,
+        }
+    },
+    earth:  {
+        location: 'Turner High School',
+        position: {
+          lat: 39.33588,
+          lng: -94.42957,
+        }
+    },
+    mars:  {
+        location: 'Sporting KC Park',
+        position: {
+          lat: 39.71699,
+          lng: -94.492403,
+      }
+    },
+    ceres:  {
+        location: 'Clinton Lake - Hickory Campground',
+        position: {
+          lat: 38.542862,
+          lng: -95.222534,
+      }
+    },
     jupiter:  {
-      location: 'Kansas State Capitol Visitor Center',
+        location: 'Kansas State Capitol Visitor Center',
+        position: {
+          lat: 39.048257,
+          lng: -95.677630
+      }
+    },
+    saturn:  {
+        location: 'Milford Lake - Acorns Resort',
+        position: {
+          lat: 39.92204,
+          lng: -96.535403,
+      }
+    },
+    uranus:  {
+        location: 'Gorham, KS',
+        position: {
+          lat: 38.525410,
+          lng: -99.12333,
+      }
+    },
+    neptune:  {
+        location: 'Goodland, KS',
+        position: {
+          lat: 39.21297,
+          lng: -101.423666,
+      }
+    },
+    pluto:  {
+      location: 'High Plains Raceway - Deer Trail, CO',
       position: {
-        lat: 39.048257,
-        lng: -95.677630
+        lat: 39.44669,
+        lng: -103.534422,
       }
     },
     userMark: {
